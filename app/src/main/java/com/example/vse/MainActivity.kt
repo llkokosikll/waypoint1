3cfd3f3b6420fc2b320c3b2135ace2bd8be21b15
llkokosikll <147972423+llkokosikll@users.noreply.github.com>
2023-10-15 11:54:06 +0300

    test
    test

---
+++ app/src/main/java/com/example/vse/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.vse
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun OnClickGoMain2(view: View){
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+    }
+}
